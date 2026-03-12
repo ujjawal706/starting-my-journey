89d2f3311c50bd0a07e6d22ce22e6422fe949316
ujjawal706 <ujjawalkumar70615@gmail.com>
2026-03-12 20:11:02 +0530

    tut2.cpp is for variables and integers
    today i learnt about variables and  integers and char.

---
+++ tut2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // int a = 8;
+    // int b = 19;
+    int a=8, b=19;
+    char c = 'v';
+    cout<< "this is to learn. \nhere the value of a is "<<a<<".";
+    cout <<" \nthe value of b is : "<< b;
+    cout <<" \nthe value of c is : "<< c;
+    return 0;
+}    
